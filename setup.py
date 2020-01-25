b52fc9ad01a388913ac0da9df9e2a53b94750fa4
Lebbenszki Attila <ati703@gmail.com>
2020-01-25 14:22:50 +0100

    Base setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ElevatorSimulator',
+      version='1.0',
+      description='Projektmunka 2 feladat',
+      url='https://github.com/Teaching-projects/SZE-ProjektMunka2-2019-Liftszimulator',
+      author='Kocsis Patrik, Lebbenszki Attila, Szerdahelyi Richárd',
+      author_email='email1@gmail.com, email2@gmail.com, email3@gmail.com',
+      install_requires=[
+          'pygame == 1.9.6',
+      ]
+      )
