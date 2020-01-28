0a5170791e7f7604c2de9e3dc50761d5d3b03f8a
szerdahelyir <tajmon1357@gmail.com>
2020-01-28 13:49:48 +0100

    tkinter to config
    tkinter moved to config

--- config.py
+++ config.py
@@ -1,14 +1,19 @@
 import pygame
+import tkinter as tk
 
 pygame.init()
 pygame.display.set_caption('Elevator simulator')
 
+
+
+
 SIZE            = [1200, 600]
-FLOORNUMBER     = 10
-ELEVATORNUMBER  = 5
-PASSENGERNUMBER = 20
+FLOORNUMBER     = None
+ELEVATORNUMBER  = None
+PASSENGERNUMBER = None
 FPS             = 60
-FLOORHEIGHT     = SIZE[1] / FLOORNUMBER
+FLOORHEIGHT     = None
+SEED            = None
 CLOCK           = pygame.time.Clock()
 SCREEN          = pygame.display.set_mode(SIZE)
 SMALLFONT       = pygame.font.SysFont(None, 16)
@@ -20,3 +25,74 @@ BLUE  = (  0,   0, 255)
 WHITE = (255, 255, 255)
 
 
+def menu():
+    root = tk.Tk()
+    tk.Label(root, text = "Emeletek szama").grid(row = 0, sticky = tk.W)
+    tk.Label(root, text = "Liftek szama").grid(row = 1, sticky = tk.W)
+    tk.Label(root, text = "Utasok szama").grid(row = 2, sticky = tk.W)
+    tk.Label(root, text = "Random seed").grid(row = 3, sticky = tk.W)
+
+    EmeletSzam = tk.Entry(root)
+    LiftSzam = tk.Entry(root)
+    UtasSzam = tk.Entry(root)
+    RandomSeed = tk.Entry(root)
+
+
+    EmeletSzam.grid(row = 0, column = 1)
+    LiftSzam.grid(row = 1, column = 1)
+    UtasSzam.grid(row = 2, column = 1)
+    RandomSeed.grid(row = 3, column = 1)
+
+    def getInput():
+
+        a = EmeletSzam.get()
+        b = LiftSzam.get()
+        c = UtasSzam.get()
+        d = RandomSeed.get()
+        e = v.get()
+        root.destroy()
+
+        
+        global FLOORNUMBER
+        FLOORNUMBER=int(a)
+        global ELEVATORNUMBER
+        ELEVATORNUMBER=int(b)
+        global PASSENGERNUMBER
+        PASSENGERNUMBER=int(c)
+        global FLOORHEIGHT
+        FLOORHEIGHT=SIZE[1] / FLOORNUMBER
+        global SEED
+        SEED=int(d)
+
+    v = tk.IntVar()
+    v.set(0)  # initializing the choice, i.e. Python
+
+    choices = [
+        ("Algoritmus 1"),
+        ("Algoritmus 2"),
+        ("Algoritmus 3"),
+        ("Manualis"),
+    ]
+
+    def ShowChoice():
+        print(v.get())
+
+    tk.Label(root, 
+            text="""Liftet vezerlo algoritmus kivalasztasa:""",
+            justify = tk.LEFT,
+            padx = 20).grid(row = 4, sticky = tk.W)
+
+    for val, choices in enumerate(choices):
+        tk.Radiobutton(root, 
+                    text=choices,
+                    padx = 20, 
+                    variable=v, 
+                    command=ShowChoice,
+                    value=val).grid(row = 5+val, sticky = tk.W)
+
+
+
+    tk.Button(root, text = "Submit",
+            command = getInput).grid(row = 10, sticky = tk.W)
+
+    root.mainloop()
--- main.py
+++ main.py
@@ -2,79 +2,16 @@ import building
 import config
 import pygame
 import csv
-import tkinter as tk
 
+
+config.menu()
+print(config.PASSENGERNUMBER)
 DONE = False
 
 Base = building.Building(config.FLOORNUMBER, config.ELEVATORNUMBER)
 Base.addPassenger(config.PASSENGERNUMBER)
 
-root = tk.Tk()
-tk.Label(root, text = "Emeletek szama").grid(row = 0, sticky = tk.W)
-tk.Label(root, text = "Liftek szama").grid(row = 1, sticky = tk.W)
-tk.Label(root, text = "Utasok szama").grid(row = 2, sticky = tk.W)
-tk.Label(root, text = "Random seed").grid(row = 3, sticky = tk.W)
 
-EmeletSzam = tk.Entry(root)
-LiftSzam = tk.Entry(root)
-UtasSzam = tk.Entry(root)
-RandomSeed = tk.Entry(root)
-
-
-EmeletSzam.grid(row = 0, column = 1)
-LiftSzam.grid(row = 1, column = 1)
-UtasSzam.grid(row = 3, column = 1)
-RandomSeed.grid(row = 2, column = 1)
-
-def getInput():
-
-    a = EmeletSzam.get()
-    b = LiftSzam.get()
-    c = UtasSzam.get()
-    d = RandomSeed.get()
-    e = v.get()
-    root.destroy()
-
-    global params
-    params = [a,b,c,d,e]
-
-v = tk.IntVar()
-v.set(0)  # initializing the choice, i.e. Python
-
-choices = [
-    ("Algoritmus 1"),
-    ("Algoritmus 2"),
-    ("Algoritmus 3"),
-    ("Manualis"),
-]
-
-def ShowChoice():
-    print(v.get())
-
-tk.Label(root, 
-         text="""Liftet vezerlo algoritmus kivalasztasa:""",
-         justify = tk.Left,
-         padx = 20).grid(row = 4, sticky = tk.W)
-
-for val, choices in enumerate(choices):
-    tk.Radiobutton(root, 
-                  text=choices,
-                  padx = 20, 
-                  variable=v, 
-                  command=ShowChoice,
-                  value=val).grid(row = 5+val, sticky = tk.W)
-
-
-
-tk.Button(root, text = "Submit",
-           command = getInput).grid(row = 10, sticky = tk.W)
-
-
-
-
-
-root.mainloop()
-print(params)
 
 
 while not DONE:
